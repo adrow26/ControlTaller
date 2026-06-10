0cbc53507d44a04732ef6e6fb0583250aeec7726
adrow26 <adrow.srl@gmail.com>
2026-06-10 03:19:10 -0400

    Update taller.py

--- taller.py
+++ taller.py
@@ -14,7 +14,8 @@ def conectar_db():
             password TEXT NOT NULL
         )
     ''')
-    def crear_tablas_taller():
+    
+def crear_tablas_taller():
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     
