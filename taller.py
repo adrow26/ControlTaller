a0e9537f5a4ebd96bc29381950357f8875db6875
adrow26 <adrow.srl@gmail.com>
2026-06-10 00:30:55 -0400

    Update taller.py

--- taller.py
+++ taller.py
@@ -1,13 +1,13 @@
 import flet as ft
 import os
 
-async def main(page: ft.Page):
+def main(page: ft.Page):
     page.title = "Control Taller"
     page.add(ft.Text("App Taller lista ✅", size=30))
 
-app = ft.app_async(
+ft.app(
     target=main,
     port=int(os.getenv("PORT", 8080)),
     host="0.0.0.0",
-    return_app=True  # <- esta línea es la clave
+    view=None  # <- sin esto Railway no abre navegador
 )
