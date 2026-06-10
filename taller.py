ac6a3bdf933f1efe3b6a46b620e53f78ca95a548
adrow26 <adrow.srl@gmail.com>
2026-06-09 22:44:22 -0400

    Update taller.py

--- taller.py
+++ taller.py
@@ -1,7 +1,9 @@
 import flet as ft
+import os
 
 def main(page: ft.Page):
     page.title = "Control Taller"
     page.add(ft.Text("App Taller lista ✅", size=30))
 
+if __name__ == "__main__":
 ft.app(target=main, port=8080, host="0.0.0.0")
