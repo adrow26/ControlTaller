ed33995c3512be7d4d8f7f9d9989b347d779ebec
adrow26 <adrow.srl@gmail.com>
2026-06-09 23:59:52 -0400

    Update taller.py

--- taller.py
+++ taller.py
@@ -1,7 +1,12 @@
 import flet as ft
+import os
 
 async def main(page: ft.Page):
     page.title = "Control Taller"
     page.add(ft.Text("App Taller lista ✅", size=30))
 
-app = ft.app_async(target=main)
+app = ft.app_async(
+    target=main,
+    port=int(os.environ["PORT"]),
+    host="0.0.0.0"
+)
