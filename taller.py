3ee0db01e7298ba52b4eb0de67e93002a78bf5ea
adrow26 <adrow.srl@gmail.com>
2026-06-09 23:09:07 -0400

    Update taller.py

--- taller.py
+++ taller.py
@@ -5,6 +5,4 @@ def main(page: ft.Page):
     page.title = "Control Taller"
     page.add(ft.Text("App Taller lista ✅", size=30))
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    ft.app(target=main, port=port, host="0.0.0.0")
+ft.app(target=main, port=int(os.getenv("PORT", 8080)), host="0.0.0.0", view=None)
